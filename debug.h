4fe1677f28246607784ce0c4496c4942f315fe14
Daniel Klopp <daniel.klopp.1989@gmail.com>
2017-08-20 14:09:20 +0000

    Add files via upload

--- debug.h
+++ debug.h
@@ -1,10 +1,6 @@
 #ifndef debug_h
 #define debug_h
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-
 #define DBACT_COUNT 7
 
 #define END         0
@@ -24,81 +20,12 @@
 		                                &&dbact_print_stk, \
 		                                &&dbact_print_mem}
 
-const char* dbmenu_str = "\n\t[0]end [1]run [2]step [3]stop [4]reset [5]print-stk [6]print-mem";
-const char* input_msg  = "\n --> ";
-const char* invalid_input_msg = "\n\tinvalid input, try again.";
+const char* dbmenu_str;
+const char* input_msg;
+const char* invalid_input_msg;
 
-u8
-is_int(const char* str)
-{
-	u8 i, len = strlen(str);
+u8  is_int(const char*);
+u8* get_stdin_str();
+u8  dbmenu_input();
 
-	// Cant be valid if length less than one.
-	if (len < 1)
-		return 0;
-
-	// If number begins with a zero it's
-	// invalid unless it's a lone zero,
-	// if we do detect a lone zero we return 1 now.
-	if (*str == '0') {
-		if (len == 1)
-			return 1;
-		else
-			return 0;
-	}
-
-	// Inspect string char at a time, if we find
-	// a non-digit character then is invalid integer.
-	for (i=0; i < len; ++i) {
-		if (!isdigit(str[i]))
-			return 0;
-	}
-
-	// Return 1 to signal that str represents an integer.
-	return 1;
-}
-
-u8* get_stdin_str() {
-    u8  buf[256];
-    u8  sz = 0;
-    u8* r;
-    int ch;
-    while ((ch=fgetc(stdin)) != 10)
-        buf[sz++] = (u8) ch;
-    buf[sz] = 0;
-    r = (u8*) malloc(sz+1);
-    strcpy(r, buf);
-    return r;
-}
-
-
-u8 dbmenu_input()
-{
-    int input;
-	u8* str;
-
-	for (;;) {
-		printf("%s%s", dbmenu_str, input_msg);
-		str = get_stdin_str();
-		if (strlen(str) == 1) {
-		    if (is_int(str)) {
-		    	input = atoi(str);
-		    	free(str);
-			    switch (input) {
-				    case END: case RUN: case STEP: case STOP: case RESET: case PRINT_STK: case PRINT_MEM:
-					    return (u8) input;
-				    default:
-					    printf("%s", invalid_input_msg);
-					    continue;
-			    }
-			} else {
-			    printf("%s", invalid_input_msg);
-			    continue;		
-		    }
-		} else {
-		    printf("%s", invalid_input_msg);
-			continue;	
-		} 
-	}
-}
 #endif
